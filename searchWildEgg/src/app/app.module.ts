f338e7c5eb4cce4a459fcb9aac41a4f72d9ecd29
Benjamin Vincent <benjamin.rm.vincent@gmail.com>
2019-04-18 15:20:36 +0200

    Ajout HOME, MAP, COLLECTION

--- searchWildEgg/src/app/app.module.ts
+++ searchWildEgg/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { CollectionComponent } from './collection/collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
