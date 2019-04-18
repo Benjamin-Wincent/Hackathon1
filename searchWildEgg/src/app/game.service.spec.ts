2cd5b760591d58305a3482719d252742b8f98ad5
Benjamin Vincent <benjamin.rm.vincent@gmail.com>
2019-04-18 22:52:32 +0200

    MAJ

--- searchWildEgg/src/app/game.service.spec.ts
+++ searchWildEgg/src/app/game.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CheminService } from './chemin.service';
+import { GameService } from './game.service';
 
-describe('CheminService', () => {
+describe('GameService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: CheminService = TestBed.get(CheminService);
+    const service: GameService = TestBed.get(GameService);
     expect(service).toBeTruthy();
   });
 });
---
+++ searchWildEgg/src/app/game.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { MapService } from './map.service';
+import { AssetService } from './asset.service';
+
+// const createEggs = (img, number) =>
+// {
+// map
+  // return // <button id="eggNumber"><img /></button>
+// }
+// tu veut créer une methode qui va créer des eggs button avec une ID différentes, en params tu peut lui 
+// donner une img, un numéros.
+// const createEggs1 = ( img ) => {
+  // let egg = img
+  // egg.map()
+// }
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GameService {
+
+
+  constructor(public map : MapService, public asset : AssetService) { }
+
+onEggClick() {
+  if(this.asset.assets = this.asset.getAsset()) {
+    console.log("yes")
+  }
+  else{
+    console.log('no')
+  }
+}
+}
